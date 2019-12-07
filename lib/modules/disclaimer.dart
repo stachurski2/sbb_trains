59fefb0654005a17e35a5cb5419153f4ec985c77
Stanislaw Sobczyk <stanislawsobczyk@icloud.com>
2019-12-07 16:16:29 +0100

    addded padding to ok button in disclaimer module

--- lib/modules/disclaimer.dart
+++ lib/modules/disclaimer.dart
@@ -64,6 +64,9 @@ class Disclaimer extends StatelessWidget {
                 ),
               ],
             ),
+            SizedBox(
+              height: 20,
+            )
           ],
         )
 
